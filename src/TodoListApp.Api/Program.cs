990bd21ec07d4dded82259c0fa5167194153093a
Ali111q <ali.hazim@dtc.iq>
2025-09-21 13:43:03 +0300

    fix: update CORS policy to allow any origin and simplify configuration

--- src/TodoListApp.Api/Program.cs
+++ src/TodoListApp.Api/Program.cs
@@ -18,11 +18,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials()
-              .SetIsOriginAllowedToAllowWildcardSubdomains();
+              .AllowAnyMethod();
     });
 });
 
